7ff0adc6a5b4024db90d918b32cb2b7fa4d69c42
Ubuntu <ubuntu@ip-172-31-19-212.us-west-2.compute.internal>
2014-10-07 15:21:42 +0000

    Update routes for show/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  # an individual high_score object
   get 'high_scores/:id', to: 'high_scores#show', as: 'high_score'
 
+  # all high_score objects
   get 'high_scores', to: 'high_scores#index', as: 'high_scores'
 
   root 'high_scores#index'
