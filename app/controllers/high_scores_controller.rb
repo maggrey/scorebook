42d5b96b9d84f47c5937f448a668e2a4a8f2ad5f
Ubuntu <ubuntu@ip-172-31-19-212.us-west-2.compute.internal>
2014-10-12 01:19:01 +0000

    Completes step 15

--- app/controllers/high_scores_controller.rb
+++ app/controllers/high_scores_controller.rb
@@ -10,4 +10,49 @@ class HighScoresController < ApplicationController
     @high_score = HighScore.find(params[:id])
   end
 
+
+  # GET /high_scores/new
+  def new
+    @high_score = HighScore.new
+  end
+
+  # POST /high_scores
+  def create
+    # the require function pulls the sub-hash :high_score 
+    #   out of the entire params hash
+    # the permit functions limits the considered 
+    #   parameters so a malicious user from 
+    # cannot inject additional <field,value> entries into the params hash
+    @high_score = HighScore.new(params.require(:high_score).permit(:user, :game, :score))
+
+    if @high_score.save
+      redirect_to @high_score, notice: 'High score was successfully created.  Fucking yeah!'
+    else
+      render :new
+    end
+  end
+
+  # GET /high_scores/1/edit
+  def edit
+    @high_score = HighScore.find(params[:id])
+  end
+
+  # PATCH/PUT /high_scores/1
+  def update
+    @high_score = HighScore.find(params[:id])
+    if @high_score.update(params.require(:high_score).permit(:user, :game, :score))
+      redirect_to @high_score, notice: 'High score was successfully updated.' 
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /high_scores/1
+  def destroy
+    @high_score = HighScore.find(params[:id])
+    @high_score.destroy
+      redirect_to high_scores_url, notice: 'High score was successfully destroyed.' 
+  end
+
+
 end
--- app/models/high_score.rb
+++ app/models/high_score.rb
@@ -1,5 +1,8 @@
 class HighScore < ActiveRecord::Base 
 
-
+  validates :user, presence: true 
+  validates :game, presence: true 
+  validates :score, presence: true, 
+                    numericality: {greater_than_or_equal_to: 0}
 
 end
